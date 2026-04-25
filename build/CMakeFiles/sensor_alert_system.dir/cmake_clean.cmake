3b33af8d74ef1ef62ae3f6e9b35ef0d8c1930a12
Yagizhan <deyagizhan@gmail.com>
2026-04-25 13:44:10 +0300

    build: add initial CMake project setup

---
+++ build/CMakeFiles/sensor_alert_system.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sensor_alert_system.dir/link.d"
+  "CMakeFiles/sensor_alert_system.dir/src/main.cpp.o"
+  "CMakeFiles/sensor_alert_system.dir/src/main.cpp.o.d"
+  "sensor_alert_system"
+  "sensor_alert_system.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sensor_alert_system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    std::cout << "Sensor Data Processing and Alert Managment System" << std::endl;
+    std::cout << "Developers: Yagizhan Demir - Burak Dinarli" << std:: endl;
+    std::cout << "Status: Project initialized succesfully." << std::endl;
+
+    return 0;
+}
