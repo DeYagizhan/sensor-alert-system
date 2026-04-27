8b52a24f522b491eaf67cc77e6905a423fb2cbdb
Yagizhan <deyagizhan@gmail.com>
2026-04-27 17:02:13 +0300

    chore: remove build artifacts from repository

--- build/CMakeFiles/sensor_alert_system.dir/src/core/ThresholdRule.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/sensor_alert_system.dir/src/core/ThresholdRule.cpp.o: \
- /home/yagizhan/projects/sensor-alert-system/src/core/ThresholdRule.cpp \
- /usr/include/stdc-predef.h \
- /home/yagizhan/projects/sensor-alert-system/include/sensor_alert/core/ThresholdRule.hpp
